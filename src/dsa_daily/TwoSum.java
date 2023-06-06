771e701421d4cde18c2068551b69330e0eb6bffc
Shambhu Gohel <gohelshambhu95@yahoo.com>
2023-06-06 23:58:29 +0530

    Added Solution

--- src/dsa_daily/TwoSum.java
+++ src/dsa_daily/TwoSum.java
@@ -1,5 +1,9 @@
 package dsa_daily;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
     /*
         Given an array of integers nums and an integer target , return indices
@@ -17,8 +21,18 @@ public class TwoSum {
         int[] array = {1, 2, 4, 5, 8, 5, 8, 9, 5, 6};
         int target = 13;
         int[] result = getIndexBruteforce(array, target);
-        for (int i = 0; i < result.length; i++) {
-            System.out.println(result[i]);
+        int[] result2 = getIndexUsingHashMap(array, target);
+        int[] result3 = getIndexUsingTwoPointer(array, target);
+        for (int j : result) {
+            System.out.print(j + " ");
+        }
+
+        for (int j : result2) {
+            System.out.print(j + " ");
+        }
+
+        for (int j : result3) {
+            System.out.print(j + " ");
         }
     }
 
@@ -26,7 +40,6 @@ public class TwoSum {
         int[] result = new int[2];
         for (int i = 0; i < input.length; i++) {
             for (int j = i + 1; j < input.length; j++) {
-                System.out.println("M");
                 int sum = input[i] + input[j];
                 if (sum == target) {
                     result[0] = i;
@@ -37,5 +50,34 @@ public class TwoSum {
         }
 
         return result;
+    }
+
+    public static int[] getIndexUsingHashMap(int[] input, int target) {
+        Map<Integer, Integer> result = new HashMap<>();
+        for (int i = 0; i < input.length; i++) {
+            int copl = target - input[i];
+            if (result.containsKey(copl)) {
+                return new int[]{result.get(copl), i};
+            } else {
+                result.put(input[i], i);
+            }
+        }
+        return new int[]{};
+    }
+
+    public static int[] getIndexUsingTwoPointer(int[] input, int target) {
+        Arrays.sort(input);
+        int left = 0;
+        int right = input.length - 1;
+        while (left < right) {
+            if (input[left] + input[right] == target) {
+                return new int[]{input[left], input[right]};
+            } else if (input[left] + input[right] < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return new int[]{};
     }
 }
