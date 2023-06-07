28f427647a78c2cc8baa650a6c6cddc8487d4c04
Shambhu Gohel <gohelshambhu95@yahoo.com>
2023-06-07 23:59:34 +0530

    ValidParentheses

---
+++ src/dsa_daily/ValidParentheses.java
@@ -0,0 +1,25 @@
+package dsa_daily;
+
+public class ValidParentheses {
+    public static boolean isValidParentheses(String input) {
+
+        if (input.isEmpty()) {
+            return true;
+        } else {
+            for (int i = 0; i < input.length() - 1; i++) {
+                if ((input.charAt(i) == '(' && input.charAt(i + 1) == ')') ||
+                        (input.charAt(i) == '{' && input.charAt(i + 1) == '}') ||
+                        (input.charAt(i) == '[' && input.charAt(i + 1) == ']')) {
+                    input = input.substring(0, i) + input.substring(i + 2);
+                    System.out.println("Input is " + input);
+                    isValidParentheses(input);
+                }
+            }
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValidParentheses("()[]{}"));
+    }
+}
